af827daaec9a19518e7007dc449e2ee861deaf42
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-18 21:43:44 +0200

    initial commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Good Day sir\nYou are now Logged in\n: )", style: TextStyle(fontSize: 20),)),
+    );
+  }
+}
