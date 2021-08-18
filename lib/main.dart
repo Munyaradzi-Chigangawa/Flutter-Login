650f2286aacc0a3a8ca04ce17591289800615739
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-18 21:37:40 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login/screens/welcome.dart';
+
+// ignore: invalid_language_version_override
+// @dart=2.9
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WelcomeScreen
+      (),
+    );
+  }
+}
