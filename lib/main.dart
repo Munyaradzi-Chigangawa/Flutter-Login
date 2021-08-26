c2667490a8e3b22309166052db73671d21dbc048
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-26 18:17:16 +0200

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_login/screens/welcome.dart';
+import 'screens/login.dart';
 
-// ignore: invalid_language_version_override
-// @dart=2.9
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -21,8 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomeScreen
-      (),
+      home: Login(),
     );
   }
 }
